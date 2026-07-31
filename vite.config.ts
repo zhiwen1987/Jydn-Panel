87fca4d0550cd2b4e60b2ab5f3eecf082eddda64
zhiwen1987 <zhiwen2528@163.com>
2026-07-31 09:50:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ function setupPlugins(env: ImportMetaEnv): PluginOption[] {
     env.VITE_GLOB_APP_PWA === 'true' && VitePWA({
       injectRegister: 'auto',
       manifest: {
-        name: 'AnGe-Panel',
-        short_name: 'AnGe-Panel',
+        name: 'Jydn-Panel',
+        short_name: 'Jydn-Panel',
         icons: [
           { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
           { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
